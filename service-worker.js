329e9b954195c6b970e8e4c5f416e42873ca85cf
崔海峰 <cuihaifeng@qutoutiao.net>
2019-02-19 14:39:59 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ importScripts("workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "workbox-v3.6.3"});
 
 importScripts(
-  "precache-manifest.8edd3b181e22beca970fc09ad15843c4.js"
+  "precache-manifest.d9c828278e370ccbc30012403f376f9b.js"
 );
 
 workbox.skipWaiting();
